a2633f0ad610eeb09b6e576f5f7b1af17f8b9c80
Crowdbotics <team@crowdbotics.com>
2021-09-10 11:36:40 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "billowing_salad_30433Identifier",
+  url: "https://billowing_salad_30433Identifier.botics.co",
+  api: "https://billowing_salad_30433Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
